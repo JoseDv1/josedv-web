c429be5942aaf73f0c3e279600eca182d308dd76
JoseDv1 <jose123millon@gmail.com>
2023-10-02 19:39:06 -0500

    Change extesion from easy file to astro

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,39 +1,39 @@
 <script>
-	import ProjectItem from './ProjectItem.svelte';
+	import ProjectItem from "./ProjectItem.svelte";
 
 	let items = [
 		{
-			title: 'Melodimatch',
+			title: "Melodimatch",
 			description:
-				'MelodiMatch es una aplicación, desarrollada en Svelte con la API de Spotify, de descubrimiento musical que te ayuda a encontrar nueva música basada en tus artistas y canciones favoritos, guarda las canciones que descubres y crea una lista de reproducción con ellas.',
-			img: '/Melodimatch.webp',
-			live: 'https://melodimatch.vercel.app/',
-			github: 'https://github.com/JoseDv1/Melodimatch'
+				"MelodiMatch es una aplicación, desarrollada en Svelte con la API de Spotify, de descubrimiento musical que te ayuda a encontrar nueva música basada en tus artistas y canciones favoritos, guarda las canciones que descubres y crea una lista de reproducción con ellas.",
+			img: "/Melodimatch.webp",
+			live: "https://melodimatch.vercel.app/",
+			github: "https://github.com/JoseDv1/Melodimatch",
 		},
 		{
-			title: 'Stone Drinks',
+			title: "Stone Drinks",
 			description:
-				'Una landing page hecha en react para mi bar favorito, Stone Drinks, donde puedes ver el menú y promociones',
-			img: '/StoneDrinks.webp',
-			live: 'https://stone-drinks-web.vercel.app/',
-			github: 'https://github.com/JoseDv1/StoneDrinks-Web'
+				"Una landing page hecha en react para mi bar favorito, Stone Drinks, donde puedes ver el menú y promociones",
+			img: "/StoneDrinks.webp",
+			live: "https://stone-drinks-web.vercel.app/",
+			github: "https://github.com/JoseDv1/StoneDrinks-Web",
 		},
 		{
-			title: 'NaturAle App',
+			title: "NaturAle App",
 			description:
-				'NaturAle es una aplicación de Nutricion y Salud desarrollada en Flutter con Dart y Firebase, donde puedes crear una cuenta, iniciar sesión, crear un perfil, agregar alimentos a tu diario y recibir asesorias de Ale',
-			img: '/NaturAle.webp',
-			live: '',
-			github: 'https://github.com/JoseDv1/NaturAle'
+				"NaturAle es una aplicación de Nutricion y Salud desarrollada en Flutter con Dart y Firebase, donde puedes crear una cuenta, iniciar sesión, crear un perfil, agregar alimentos a tu diario y recibir asesorias de Ale",
+			img: "/NaturAle.webp",
+			live: "",
+			github: "https://github.com/JoseDv1/NaturAle",
 		},
 		{
-			title: 'Cris Tales',
+			title: "Cris Tales",
 			description:
-				'Este proyecto es un clon de la página oficial del videojuego Cris Tales, desarrollado en Svelte con SvelteKit',
-			live: 'https://clon-cristales-svelte.vercel.app/',
-			github: 'https://github.com/JoseDv1/clon-cristales-svelte',
-			img: '/CrisTales.webp'
-		}
+				"Este proyecto es un clon de la página oficial del videojuego Cris Tales, desarrollado en Svelte con SvelteKit",
+			live: "https://clon-cristales-svelte.vercel.app/",
+			github: "https://github.com/JoseDv1/clon-cristales-svelte",
+			img: "/CrisTales.webp",
+		},
 	];
 </script>
 
