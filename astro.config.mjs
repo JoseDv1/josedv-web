63ead8b979e01f277cd3553be1ae7b849d25da45
JoseDv1 <jose123millon@gmail.com>
2023-10-04 12:39:35 -0500

    Add Sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+	site: "https://josedv.com",
+	integrations: [svelte(), tailwind(), sitemap()],
 });
