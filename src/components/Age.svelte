2eee3cd9a6a8cf3270f366041399ad15b22de1d0
Jose Villegas <jose123millon@gmail.com>
2023-10-17 22:47:30 -0500

    Add age component to About page

---
+++ src/components/Age.svelte
@@ -0,0 +1,9 @@
+<script>
+	const FechaActual = new Date().getTime();
+	const FechaCumple = new Date(2003, 9, 4).getTime();
+	let edad = Math.floor(
+		(FechaActual - FechaCumple) / (1000 * 60 * 60 * 24 * 365.25)
+	);
+</script>
+
+<span>{edad} <A:tel />ños</span>
